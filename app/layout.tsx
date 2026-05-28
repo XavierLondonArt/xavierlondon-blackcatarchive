c9ede66afd1d8243dc144ae7eee9e65e58be3d22
L Galloway <londongalloway415@gmail.com>
2026-05-28 12:46:10 -0700

    Vercel analytics added to the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { JetBrains_Mono } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/next";
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
 
 export const metadata: Metadata = {
   title: "xavierlondon.art",
@@ -20,9 +24,19 @@ export default function RootLayout({
     <html lang="en" className={cn("font-mono", jetbrainsMono.variable)}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
       </head>
-      <body suppressHydrationWarning>{children}</body>
+
+      <body suppressHydrationWarning>
+        {children}
+
+        {/* Vercel Analytics */}
+        <Analytics />
+      </body>
     </html>
   );
 }
